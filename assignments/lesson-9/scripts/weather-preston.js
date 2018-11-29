52d12e1fa9be37e423b9d9848c2ea6316b76440f
Mac17028 <mac17028@byui.edu>
2018-11-29 11:24:15 -0700

    Several Changes API and Links L9

---
+++ assignments/lesson-9/scripts/weather-preston.js
@@ -0,0 +1,12 @@
+var weatherRequest = new XMLHttpRequest();
+var apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=ecb60c79e171db217892f089857b991b";
+
+weatherRequest.open('GET', apiURL, true);
+weatherRequest.responseType = "text";
+weatherRequest.send();
+
+weatherRequest.onload =  function () {
+    var weatherData = JSON.parse(weatherRequest.response);
+    console.log(weatherData);
+    document.getElementById('currenTemp').innerHTML = weatherData.main.temp;
+}
