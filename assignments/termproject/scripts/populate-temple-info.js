10bd61f2bb4f1f56087cb0314bf02f6a986fa781
Mac17028 <mac17028@byui.edu>
2018-12-18 15:02:23 -0700

    Temple page (finally)

--- assignments/termproject/scripts/populate-temple-info.js
+++ assignments/termproject/scripts/populate-temple-info.js
@@ -1,3 +1,91 @@
+var slideIndex = 0;
+
+// Prev and next buttons functionality
+$(function(){
+    $('.prev').click(function(){
+        showSlide(slideIndex -= 1);
+        $(".templeServices, .templeSchedule, .closureDates").empty();
+        refreshTempleInfo();
+    });
+
+    $('.next').click(function(){
+        showSlide(slideIndex += 1);
+        $(".templeServices, .templeSchedule, .closureDates").empty();
+        refreshTempleInfo();
+    });
+});
+
+function refreshTempleInfo() {
+    var infoRequest = new XMLHttpRequest();
+    var templeInfoAPIURL = "https://mac17028.github.io/assignments/termproject/scripts/temple-info.json";
+
+    infoRequest.open('GET', templeInfoAPIURL, true);
+    infoRequest.responseType = "text";
+    infoRequest.send();
+
+    infoRequest.onload = function() {
+        var jsonTempleInfo = JSON.parse(infoRequest.response);
+        populateTempleInfo(jsonTempleInfo);
+    }
+}
+
+function showSlide(n) {
+    if (n < 0) {
+        document.getElementById('templeName').innerHTML = "Jordan River Utah Temple";
+        refreshWeatherInfo("https://api.openweathermap.org/data/2.5/weather?id=5781770&units=imperial&APPID=ecb60c79e171db217892f089857b991b");
+        $(function(){
+            $('.templeImage > img').attr('src', '/assignments/termproject/images/temples/jordan-river/jordan-river-1.jpg');
+        });
+        slideIndex = 3;
+    } else if (n == 0) {
+        document.getElementById('templeName').innerHTML = "Salt Lake Temple";
+        refreshWeatherInfo("https://api.openweathermap.org/data/2.5/weather?id=5780993&units=imperial&APPID=ecb60c79e171db217892f089857b991b");
+        $(function(){
+            $('.templeImage > img').attr('src', '/assignments/termproject/images/temples/salt-lake/salt-lake-1.jpg');
+        });
+    } else if (n == 1) {
+        document.getElementById('templeName').innerHTML = "Draper Utah Temple";
+        refreshWeatherInfo("https://api.openweathermap.org/data/2.5/weather?id=5774001&units=imperial&APPID=ecb60c79e171db217892f089857b991b");
+        $(function(){
+            $('.templeImage > img').attr('src', '/assignments/termproject/images/temples/draper/draper-temple-1.jpg');
+        });
+    } else if (n == 2) {
+        document.getElementById('templeName').innerHTML = "Oquirrh Mountain Utah Temple";
+        refreshWeatherInfo("https://api.openweathermap.org/data/2.5/weather?id=5781770&units=imperial&APPID=ecb60c79e171db217892f089857b991b");
+        $(function(){
+            $('.templeImage > img').attr('src', '/assignments/termproject/images/temples/oquirrh-mountain/oquirrh-mountain-1.jpg');
+        });
+    } else if (n == 3) {
+        document.getElementById('templeName').innerHTML = "Jordan River Utah Temple";
+        refreshWeatherInfo("https://api.openweathermap.org/data/2.5/weather?id=5781770&units=imperial&APPID=ecb60c79e171db217892f089857b991b");
+        $(function(){
+            $('.templeImage > img').attr('src', '/assignments/termproject/images/temples/jordan-river/jordan-river-1.jpg');
+        });
+    } else if (n > 3) {
+        document.getElementById('templeName').innerHTML = "Salt Lake Temple";
+        refreshWeatherInfo("https://api.openweathermap.org/data/2.5/weather?id=5780993&units=imperial&APPID=ecb60c79e171db217892f089857b991b");
+        $(function(){
+            $('.templeImage > img').attr('src', '/assignments/termproject/images/temples/salt-lake/salt-lake-1.jpg');
+        });
+        slideIndex = 0;
+    }
+}
+
+function refreshWeatherInfo(currentURL) {
+    var weatherRequest = new XMLHttpRequest();
+    var apiURL = currentURL;
+
+    weatherRequest.open('GET', apiURL, true);
+    weatherRequest.responseType = "text";
+    weatherRequest.send();
+
+    weatherRequest.onload = function() {
+    var jsonWeatherData = JSON.parse(weatherRequest.response);
+    populateWeatherData(jsonWeatherData);
+    }
+}
+
+// Populate weather info
 var weatherRequest = new XMLHttpRequest();
 var apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5780993&units=imperial&APPID=ecb60c79e171db217892f089857b991b";
 
@@ -11,8 +99,6 @@ weatherRequest.onload = function() {
 }
 
 function populateWeatherData(weatherData) {
-    console.log(weatherData);
-
     document.querySelector('.currentCurrently').innerHTML = weatherData.weather[0].main;
     document.querySelector('.currentTemp').innerHTML = weatherData.main.temp;
     document.querySelector('.currentHumidity').innerHTML = weatherData.main.humidity;
@@ -23,6 +109,7 @@ function populateWeatherData(weatherData) {
     document.querySelector('.currentCurrentlyImg').src = weatherIconUrl;
 }
 
+// Populate temple info
 var infoRequest = new XMLHttpRequest();
 var templeInfoAPIURL = "https://mac17028.github.io/assignments/termproject/scripts/temple-info.json";
 
@@ -36,7 +123,6 @@ infoRequest.onload = function() {
 }
 
 function populateTempleInfo(templeInfo) {
-    console.log(templeInfo);
     var templeName = document.getElementById('templeName').innerHTML;
     var templesEach = templeInfo['temples']
     var servicesSpan = document.querySelector('.templeServices');
